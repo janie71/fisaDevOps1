6daf7e9602d342f6f80099c05c666bdcddb053b6
이유진 <yjin4526@gmail.com>
2026-03-20 19:54:46 +0900

    commit

---
+++ src/main/java/edu/fisa/ce/Step06BuildApplication.java
@@ -0,0 +1,13 @@
+package edu.fisa.ce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step06BuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step06BuildApplication.class, args);
+	}
+
+}
